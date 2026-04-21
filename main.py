3f25979b130068c887406a448205a5092d89207e
mala25ctrl <l.malavolti.proget@gmail.com>
2026-04-21 22:35:25 +0200

    feat: Aggiunta metodi di ricerca, massimo, minimo, successore, transplant e cancellazione nell'albero binario di ricerca

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ if __name__ == '__main__':
     tree.insert(BSTNode(6))
 
     tree.inorder_walk()
+
+    x = tree.search(4)
+    if x is not None:
+        print("Nodo trovato:", x.key)
+
+    x = tree.maximum()
+    if x is not None:
+        print("Nodo con chiave massima:", x.key)
+
+    x = tree.minimum()
+    if x is not None:
+        print("Nodo con chiave minima:", x.key)
+
+    nodeToDelete = tree.search(5)
+    if nodeToDelete is not None:
+        tree.delete(nodeToDelete)
+        tree.inorder_walk()
